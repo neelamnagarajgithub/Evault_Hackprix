1ae1915cc35fcb83e4acfd4cde5162860443c0fe
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-06-08 12:48:08 +0530

    Hardhat Configuration Setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+require("@nomicfoundation/hardhat-toolbox") ;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const {SEPOLIA_URL,SECRET_KEY}=process.env;
+
+const config={
   solidity: "0.8.24",
+    networks: {
+       sepolia: {
+         url: SEPOLIA_URL,
+         accounts: SECRET_KEY!==undefined?[SECRET_KEY]:[] ,
+       },
+    },
 };
+
+module.exports=config;
